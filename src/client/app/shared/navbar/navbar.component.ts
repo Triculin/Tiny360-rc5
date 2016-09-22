9e851239773e3038b127247b8c2c63780560d0cc
vikram-triculin <vikram.p@triculintech.com>
2016-09-22 11:41:08 +0530

    json from service

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpService} from "../../services/http-service";
 import {HTTP_PROVIDERS} from "@angular/http";
+import {menuList} from "./menuList";
 
 /**
  * This class represents the navigation bar component.
@@ -14,9 +15,25 @@ import {HTTP_PROVIDERS} from "@angular/http";
 })
 
 export class NavbarComponent {
-  menuItems:any;
+  menuItems:menuList;
+  // cnt:boolean = false;
+  cnt:number = 0;
+  count:number = 0;
+  // submenus:any;
   constructor(public httpService: HttpService)
   {
-    this.httpService.getMasterConfig().subscribe(res =>{this.menuItems = res.menuList;});
-  }
+    this.httpService.getMasterConfig().subscribe(res =>{this.menuItems = res.menuList;});   
+}
+// getSubitems(i:menuList){
+//  this.submenus=this.menuItems[i].subMenuList;
+// }
+// toggleSubitems(abc:string){
+    toggleSubitems(i:number){
+      this.cnt=i;
+      this.count++;
+// alert(abc);
+// alert(this.menuItems.name+'name');
+
+// this.cnt = !this.cnt;
+}
 }
