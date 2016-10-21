5ef791ae3184dafa95eb16273d49185743edc2a2
itsupport <itsupport@Dark-Horse.com>
2016-10-21 17:13:19 +0530

    added multiselected dropdown in DialogComponent

--- src/client/app/shared/dialog/dialog.component.ts
+++ src/client/app/shared/dialog/dialog.component.ts
@@ -1,13 +1,29 @@
-import {  Component , Input  } from '@angular/core';
+import {  Component , Input ,ViewChild,OnInit  } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 @Component({
   selector: 'pop-up',
   templateUrl:'app/shared/dialog/dialog.component.html'
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
   public modalstatus:string = 'none';
   public modalHideMandatoryInfo:boolean = false;
   public modalHideOptionalInfo:boolean = true;
   @Input() data : any;
+  formSingle: FormGroup;
+  multipleSingle: boolean = false;
+  optionsSingle: Array<any> = [];
+  formMultiple: FormGroup;
+  multipleMultiple: boolean = true;
+  optionsMultiple: Array<any> = [];
+  allowClear: boolean = true;
+  @ViewChild('singleSelectComponent') singleSelectComponent:any;
+  @ViewChild('multipleSelectComponent') multipleSelectComponent:any;
+  ngOnInit() {
+    this.formSingle = new FormGroup({});
+    this.formSingle.addControl('selectSingle', new FormControl());
+    this.formMultiple = new FormGroup({});
+    this.formMultiple.addControl('selectMultiple', new FormControl());
+  }
   modalPopupOpen() {
     this.modalstatus = 'block';
   }
@@ -24,6 +40,8 @@ export class DialogComponent {
     this.modalstatus = 'none';
     this.modalHideMandatoryInfo = false;
     this.modalHideOptionalInfo = true;
+    this.formSingle.reset();
+    this.formMultiple.reset();
   }
   modalMandatoryInfoHide() {
         this.modalHideMandatoryInfo = true;
